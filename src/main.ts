cdf4c2afc258014ef907da7a0b921e60e24ef5e4
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2021-12-21 14:51:06 +0100

    Initial commit - Adds dynamoDB table - Tests for existence

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,36 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, aws_dynamodb as DynamoDB } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
+export interface PipelineStackProps extends StackProps{
+  branchName: string;
+}
+
+export class PipelineStackDevelopment extends Stack {
+  constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
     // define resources here...
+    new DynamoDB.Table(this, 'sessions', {
+      partitionKey: { name: 'sessionid', type: DynamoDB.AttributeType.STRING }
+  });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+// for development, use sandbox account
+const sandboxEnvironment = {
+  account: '122467643252',
+  region: 'eu-west-1',
 };
 
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+if ('BRANCH_NAME' in process.env == false) {
+  new PipelineStackDevelopment(app, 'mijnuitkering-pipeline-development',
+    {
+      env: sandboxEnvironment,
+      branchName: 'development',
+    },
+  );
+}
 
 app.synth();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,26 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { MyStack } from '../src/main';
+import { PipelineStackDevelopment } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
-
+  const stack = new PipelineStackDevelopment(app, 'test', { branchName: 'test' });
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
+});
+
+
+test('HasDynamoDB', () => {
+  const app = new App();
+  const stack = new PipelineStackDevelopment(app, 'test', { branchName: 'test' });
+  const template = Template.fromStack(stack);
+  template.resourceCountIs("AWS::DynamoDB::Table", 1);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    AttributeDefinitions: [
+      {
+        "AttributeName": "sessionid",
+        "AttributeType": "S",
+      }
+    ]
+  });
 });
