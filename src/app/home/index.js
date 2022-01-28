5550edf4c52993dec808786079500483744c9215
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-01-28 14:15:54 +0100

    feat: Transform responses (#22)
    * Create api to get uitkeringsgegevens
    * Update projen
    * Update aws cdk

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,5 +1,6 @@
 const { Session } = require('./shared/Session');
 const { render } = require('./shared/render');
+const { UitkeringsApi, FileConnector } = require('./UitkeringsApi');
 
 function redirectToLogin() {
     const response = {
@@ -18,8 +19,11 @@ exports.handler = async (event, context) => {
             return redirectToLogin();
         } 
         // Get API data
+        const api = new UitkeringsApi(session.getValue('bsn'), FileConnector);
+        const data = await api.getUitkeringen();
+        
         // render page
-        const html = await render({}, __dirname + '/templates/home.mustache');
+        const html = await render(data, __dirname + '/templates/home.mustache');
         response = {
             'statusCode': 200,
             'body': html,
