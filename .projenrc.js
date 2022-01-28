43fc9147bc1326e3563d5b7cfff8a96bc19837b8
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-01-28 15:36:19 +0100

    Downgrade cdk - newest version has circular dependency issue

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.8.0',
+  cdkVersion: '2.7.0',
   defaultReleaseBranch: 'main',
   name: 'app2',
   deps: [
