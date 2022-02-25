671f527db3d1096d122633ee320071d4a06783a4
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-02-25 10:28:05 +0100

    Import dns zone, add txt record as test

--- src/DNSStack.ts
+++ src/DNSStack.ts
@@ -1,5 +1,6 @@
-import { aws_route53, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_route53 as Route53, Stack, StackProps, aws_ssm as SSM } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Statics } from './statics';
 
 export interface DNSStackProps extends StackProps {
   branch: string;
@@ -13,8 +14,17 @@ export class DNSStack extends Stack {
       production: 'mijnuitkering',
     };
     const subdomain = subdomainMap[props.branch as keyof typeof subdomainMap] ?? 'mijn-uitkering-dev';
-    new aws_route53.HostedZone(this, 'mijnuitkering-csp', {
+    new Route53.HostedZone(this, 'mijnuitkering-csp', {
       zoneName: `${subdomain}.csp-nijmegen.nl`,
+    });
+
+    const rootZoneId = SSM.StringParameter.valueForStringParameter(this, Statics.cspRootZoneId);
+    const cspRootZone = Route53.HostedZone.fromHostedZoneId(this, 'cspzone', rootZoneId);
+    
+    new Route53.TxtRecord(this, 'test-record', {
+      zone: cspRootZone,
+      recordName: 'tstjoost',
+      values: ['ditiseentestvanjoost']
     });
   }
 }
--- src/statics.ts
+++ src/statics.ts
@@ -19,4 +19,12 @@ export abstract class Statics {
    * OpenID Connect secret name
    */
   static readonly secretOIDCClientSecret: string = '/cdk/mijn-uitkering/oidc-clientsecret';
+
+  /**
+   * Route53 Zone ID for csp-nijmegen.nl in this account.
+   * NB: This depends on the eform-project existing and having set this parameter!
+   * We need to use this zone for domain validation purposes. We need to be able to 
+   * set TXT DNS-records on the main domain.
+   */
+  static readonly cspRootZoneId: string = '/gemeente-nijmegen/formio/hostedzone/id';
 }
