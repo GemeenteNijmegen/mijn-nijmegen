85f388f21dbae03b40a3811e2545e74f9c135201
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-12 13:53:01 +0100

    Update cache policy

--- src/ApiStack.ts
+++ src/ApiStack.ts
@@ -1,7 +1,8 @@
 import * as apigatewayv2 from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { aws_secretsmanager, Stack, StackProps, Duration, aws_ssm as SSM } from 'aws-cdk-lib';
-import { Distribution, PriceClass, OriginRequestPolicy, ViewerProtocolPolicy, AllowedMethods, ResponseHeadersPolicy, HeadersFrameOption, HeadersReferrerPolicy, CachePolicy, OriginRequestHeaderBehavior } from 'aws-cdk-lib/aws-cloudfront';
+import { Distribution, PriceClass, OriginRequestPolicy, ViewerProtocolPolicy, AllowedMethods, ResponseHeadersPolicy, HeadersFrameOption, HeadersReferrerPolicy, CachePolicy
+  , OriginRequestHeaderBehavior, CacheCookieBehavior, CacheHeaderBehavior, CacheQueryStringBehavior } from 'aws-cdk-lib/aws-cloudfront';
 import { HttpOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Bucket, BlockPublicAccess, BucketEncryption } from 'aws-cdk-lib/aws-s3';
@@ -59,11 +60,20 @@ export class ApiStack extends Stack {
             'Referer',
             'Accept-Language',
             'Accept-Datetime',
+            'Authoriz'
           ),
         }),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         allowedMethods: AllowedMethods.ALLOW_ALL,
-        cachePolicy: CachePolicy.CACHING_DISABLED
+        cachePolicy: new CachePolicy(this, 'cf-caching', {
+          cachePolicyName: 'cfCachingSessionsMijnUitkering',
+          cookieBehavior: CacheCookieBehavior.all(),
+          headerBehavior: CacheHeaderBehavior.allowList('Authorization'),
+          queryStringBehavior: CacheQueryStringBehavior.all(),
+          defaultTtl: Duration.seconds(0),
+          minTtl: Duration.seconds(0),
+          maxTtl: Duration.seconds(1)
+        }),
       },
       logBucket: this.logBucket(),
     });
