f82e8748431243d144712f05dce1f5a19a6c6723
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-21 16:24:06 +0100

    Fix number of params

--- test/main.test.ts
+++ test/main.test.ts
@@ -66,7 +66,7 @@ test('StackHasParameters', () => {
   const app = new App();
   const stack = new ParameterStack(app, 'test');
   const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::SSM::Parameter', 7);
+  template.resourceCountIs('AWS::SSM::Parameter', 4);
 });
 
 
