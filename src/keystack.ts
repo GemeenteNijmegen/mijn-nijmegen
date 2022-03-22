2fc8037bd643aa098fb1bf995c4b10a1113fdb6a
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-22 16:55:05 +0100

    Store data key arn, add log retention

--- src/keystack.ts
+++ src/keystack.ts
@@ -1,5 +1,6 @@
-import { aws_kms as KMS, Stack, aws_iam as IAM } from 'aws-cdk-lib';
+import { aws_kms as KMS, Stack, aws_iam as IAM, aws_ssm as SSM } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Statics } from './statics';
 
 /**
  * For session storage a sessions-table is created in dynamoDB. Session
@@ -14,14 +15,20 @@ export class KeyStack extends Stack {
   /**
      * key for encrypting logging data
      */
-  // logKey: KMS.Key;
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-    this.key = new KMS.Key(this, 'kmskey', {
-      enableKeyRotation: true,
-      description: 'encryption key for Mijn Nijmegen',
-      alias: 'mijnnijmegen/userdata',
-    });
+    // logKey: KMS.Key;
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+        this.key = new KMS.Key(this, 'kmskey', {
+            enableKeyRotation: true,
+            description: 'encryption key for Mijn Nijmegen',
+            alias: 'mijnnijmegen/userdata'
+        });
+        
+        // Store key arn to be used in other stacks/projects
+        new SSM.StringParameter(this, 'ssm_sessions_1', {
+            stringValue: this.key.keyArn,
+            parameterName: Statics.ssmDataKeyArn,
+        });
 
     // this.logKey = new KMS.Key(this, 'logkey', {
     //     enableKeyRotation: true,
