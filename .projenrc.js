2903d2a32909db356cadc53e4ee431b2282344c3
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-24 13:20:47 +0100

    Remove uitkering-specific files

--- .projenrc.js
+++ .projenrc.js
@@ -28,9 +28,8 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'install:login': 'copyfiles -f src/shared/*.js src/app/login/shared && cd src/app/login && npm install',
     'install:auth': 'copyfiles -f src/shared/*.js src/app/auth/shared && cd src/app/auth && npm install',
     'install:home': 'copyfiles -f src/shared/*.js src/app/home/shared && cd src/app/home && npm install',
-    'install:uitkeringen': 'copyfiles -f src/shared/*.js src/app/uitkeringen/shared && cd src/app/uitkeringen && npm install',
     'install:logout': 'copyfiles -f src/shared/*.js src/app/logout/shared && cd src/app/logout && npm install',
-    'postinstall': 'npm run install:login && npm run install:auth && npm run install:home && npm run install:logout && npm run install:uitkeringen',
+    'postinstall': 'npm run install:login && npm run install:auth && npm run install:home && npm run install:logout',
   },
   eslintOptions: {
     devdirs: ['src/app/login/tests', 'src/app/auth/tests', 'src/app/home/tests', 'src/app/uitkeringen/tests', 'src/app/logout/tests', '/test', '/build-tools'],
