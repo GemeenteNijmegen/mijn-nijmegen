1cae42cf35bc453bd84520ff104eee94ec2e2c28
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-25 16:25:15 +0100

    Fix cert param path

--- src/statics.ts
+++ src/statics.ts
@@ -68,7 +68,7 @@ export abstract class Statics {
   static readonly ssmNijmegenZoneIdNew: string = '/cdk/mijn-nijmegen/zones/nijmegennl-zone-id';
   static readonly ssmNijmegenZoneNameNew: string = '/cdk/mijn-nijmegen/zones/nijmegennl-zone-name';
 
-  static readonly certificatePath: string = '/cdk/mijn-nijmegen/certificates/';
+  static readonly certificatePath: string = '/cdk/mijn-nijmegen/certificates';
   static readonly certificateArn: string = '/cdk/mijn-nijmegen/certificates/certificate-arn';
 
   static readonly ssmApiGatewayId: string = '/cdk/mijn-nijmegen/apigateway-id';
