e16347e1ec5e25e5348c334a0001a312ffa61fc1
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-04-11 16:55:39 +0200

    chore: update cdk

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.17.0',
+  cdkVersion: '2.20.0',
   cdkVersionPinning: true,
   defaultReleaseBranch: 'production',
   release: true,
