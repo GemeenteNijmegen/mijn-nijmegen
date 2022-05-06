f5b59c95643d68c7ca5ac190c5fb7a50ca328bb5
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-05-06 12:05:59 +0200

    Add read only role - Allow readonly access to lambda's + loggroups - Allow readonly access to dynamodb (excluding encrypted data)

--- src/SessionsStack.ts
+++ src/SessionsStack.ts
@@ -1,5 +1,8 @@
 import { Stack, aws_ssm as SSM, StackProps, aws_kms as KMS } from 'aws-cdk-lib';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
+import { DynamoDbReadOnlyPolicy } from './iam/dynamodb-readonly-policy';
 import { SessionsTable } from './SessionsTable';
 import { Statics } from './statics';
 
@@ -17,12 +20,22 @@ export class SessionsStack extends Stack {
   constructor(scope: Construct, id: string, props: SessionStackProps) {
     super(scope, id);
     this.sessionsTable = new SessionsTable(this, 'sessions-table', { key: props.key });
-
     // Store session table id to be used in other stacks
     new SSM.StringParameter(this, 'ssm_sessions_1', {
       stringValue: this.sessionsTable.table.tableArn,
       parameterName: Statics.ssmSessionsTableArn,
     });
 
+    this.allowAccessToReadOnlyRole();
+  }
+
+  private allowAccessToReadOnlyRole() {
+    const roleArn = StringParameter.fromStringParameterName(this, 'readonly-role', Statics.ssmReadOnlyRoleArn);
+    const role = Role.fromRoleArn(this, 'read-only-role', roleArn.stringValue);
+    role.addManagedPolicy(
+      new DynamoDbReadOnlyPolicy(this, 'read-policy', {
+        tableArn: this.sessionsTable.table.tableArn,
+      }),
+    );
   }
 }
