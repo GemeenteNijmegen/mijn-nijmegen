c042621fbfff663721081243986dd0cf275a9820
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-05-20 15:37:08 +0200

    chore: update read-only role

--- src/iam/lambda-readonly-policy.ts
+++ src/iam/lambda-readonly-policy.ts
@@ -22,6 +22,7 @@ export class LambdaReadOnlyPolicy extends ManagedPolicy {
         effect: Effect.ALLOW,
         actions: [
           'lambda:List*',
+          'lambda:GetAccountSettings'
         ],
         resources: ['*'],
       },
