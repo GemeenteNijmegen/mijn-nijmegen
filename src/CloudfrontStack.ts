4474a3cb8b1e2dccb78529ab4f84f2b98309ff0a
Marnix Dessing <m.dessing@nijmegen.nl>
2022-09-27 11:32:10 +0200

    feat: set cloudfront default root object

--- src/CloudfrontStack.ts
+++ src/CloudfrontStack.ts
@@ -160,6 +160,7 @@ export class CloudfrontStack extends Stack {
       errorResponses: this.errorResponses(),
       logBucket: this.logBucket(),
       minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      defaultRootObject: '/home',
     });
     return distribution;
   }
