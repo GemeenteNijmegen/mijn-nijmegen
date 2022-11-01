7c7b0c47a94d38363a184fb981c89a94d7ad57b7
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2022-11-01 09:59:00 +0100

    change captureAWS placement again

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -3,8 +3,7 @@ const AWSXRay = require('aws-xray-sdk');
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { homeRequestHandler } = require("./homeRequestHandler");
 
-const dynamoDBClient = new DynamoDBClient();
-AWSXRay.captureAWS(dynamoDBClient); // als dit niet werkt zie: https://github.com/aws/aws-xray-sdk-node/issues/23
+const dynamoDBClient = AWSXRay.captureAWSClient(new DynamoDBClient());
 const apiClient = new ApiClient();
 
 async function init() {
