e2d345c162986eb2c209e45994056f909972caa4
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-12-14 15:08:30 +0100

    Use old merge job trigger

--- addMergeJob.js
+++ addMergeJob.js
@@ -5,7 +5,7 @@ exports.addMergeJob = function(project) {
       pullRequests: 'write',
       contents: 'write',
     },
-    if: "github.event.workflow_run.conclusion == 'success' && contains(github.event.pull_request.labels.*.name, 'auto-merge')",
+    if: "contains(github.event.pull_request.labels.*.name, 'auto-merge')",
     steps: [
       {
         run: 'gh pr merge --auto --merge "$PR_URL"',
@@ -19,10 +19,15 @@ exports.addMergeJob = function(project) {
 
   const workflow = project.github.addWorkflow('auto-merge');
   workflow.on({
-    workflowRun: {
-      workflows: ['build'],
-      types: ['completed'],
-    }
+    pullRequestTarget: {
+      types: [
+        'labeled',
+        'opened',
+        'synchronize',
+        'reopened',
+        'ready_for_review',
+      ],
+    },
   });
 
   workflow.addJobs({ automerge: mergeJob });
