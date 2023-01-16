f94bd973c6c05c30d4b9f44efe9fc8647b6b0de7
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-01-16 13:00:48 +0100

    feat: Store username with session
    To prevent every pageview having to perform a call to the BRP-api, we
    now store the username in the session table. This reduces latency and
    cost for API-usage.

--- src/app/auth/handleRequest.js
+++ src/app/auth/handleRequest.js
@@ -1,8 +1,10 @@
 const { Session } = require('@gemeentenijmegen/session');
+const { Bsn } = require('@gemeentenijmegen/utils');
 const { Response } = require('@gemeentenijmegen/apigateway-http/lib/V2/Response');
 const { OpenIDConnect } = require('./shared/OpenIDConnect');
+const { BrpApi } = require('./BrpApi');
 
-async function handleRequest(cookies, queryStringParamCode, queryStringParamState, dynamoDBClient) {
+async function handleRequest(cookies, queryStringParamCode, queryStringParamState, dynamoDBClient, apiClient) {
     let session = new Session(cookies, dynamoDBClient);
     await session.init();
     if (session.sessionId === false) {
@@ -13,10 +15,19 @@ async function handleRequest(cookies, queryStringParamCode, queryStringParamStat
     try {
         const claims = await OIDC.authorize(queryStringParamCode, state, queryStringParamState, queryStringParamState);
         if (claims) {
-            await session.createSession({ 
-                loggedin: { BOOL: true },
-                bsn: { S: claims.sub }
-            });
+            const bsn = new Bsn(claims.sub);
+            try {
+                const username = await loggedinUserName(bsn.bsn, apiClient);
+                await session.createSession({ 
+                    loggedin: { BOOL: true },
+                    bsn: { S: bsn.bsn },
+                    username: { S: username },
+                });
+            }
+            catch (error) {
+                console.debug(error);
+                return Response.error(500);
+            }
         } else {
             return Response.redirect('/login');
         }
@@ -26,4 +37,19 @@ async function handleRequest(cookies, queryStringParamCode, queryStringParamStat
     }
     return Response.redirect('/', 302, [session.getCookie()]);
 }
+
+
+async function loggedinUserName(bsn, apiClient) {
+    try { 
+        const brpApi = new BrpApi(apiClient);
+        const brpData = await brpApi.getBrpData(bsn);
+        console.debug(brpData);
+        const naam = brpData?.Persoon?.Persoonsgegevens?.Naam ? brpData.Persoon.Persoonsgegevens.Naam : 'Onbekende gebruiker';
+        return naam;
+    } catch (error) {
+        console.error('Error getting username');
+        return 'Onbekende gebruiker';
+    }
+}
+
 exports.handleRequest = handleRequest;
