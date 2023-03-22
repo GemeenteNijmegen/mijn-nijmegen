d0eb25a317f9b2704844ba92e75170cd46db6008
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-03-22 16:44:30 +0100

    feat: Update lambda insights extension layer
    Updates to the latest supported version.

--- src/ApiFunction.ts
+++ src/ApiFunction.ts
@@ -23,7 +23,7 @@ export class ApiFunction extends Construct {
   constructor(scope: Construct, id: string, props: ApiFunctionProps) {
     super(scope, id);
     // See https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Lambda-Insights-extension-versionsx86-64.html
-    const insightsArn = 'arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:16';
+    const insightsArn = 'arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:21';
     this.lambda = new Lambda.Function(this, 'lambda', {
       runtime: Lambda.Runtime.NODEJS_18_X,
       memorySize: 512,
