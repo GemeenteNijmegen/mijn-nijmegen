d11670ad532c159f979a22a296a07de1a0cee067
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-04-18 11:07:48 +0200

    Use env variable directly for endpoint

--- src/app/auth/BrpApi.ts
+++ src/app/auth/BrpApi.ts
@@ -13,9 +13,9 @@ export class BrpApi {
 
   async init() {
     if (!process.env.BRP_API_URL) {
-      throw new Error('Could not initialize brp api as no endpoint parameter is provided in BRP_API_URL (should be SSM parameter path)');
+      throw new Error('Could not initialize brp api as no endpoint parameter is provided in BRP_API_URL');
     }
-    this.endpoint = await AWS.getParameter(process.env.BRP_API_URL);
+    this.endpoint = process.env.BRP_API_URL;
   }
 
   /**
