8e8be0465b82e5ee086a18a28ddf8a2a7f969757
Marnix Dessing <m.dessing@nijmegen.nl>
2023-08-14 16:00:44 +0200

    chore: remove obsolete configurations

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -76,28 +76,6 @@ const EnvironmentConfigurations: {[key:string]: Configuration} = {
     pipelineStackCdkName: 'mijnnijmegen-pipeline-production',
     pipelineName: 'mijnnijmegen-production',
   },
-  'acceptance-new-lz': {
-    branch: 'acceptance',
-    buildEnvironment: Statics.gnBuildEnvironment,
-    deploymentEnvironment: Statics.gnMijnNijmegenAccpEnvironment,
-    cnameRecords: {
-      _554c359f26fbc43f02d85e43dccd6336: '_430b5afffdedea75381eaec545e8189c.vrcmzfbvtx.acm-validations.aws.',
-    },
-    dsRecord: '3766 13 2 11761745E09473E6CE95DB798CF1ADB69B4433E73EEFC9F7FE341561966EA154',
-    pipelineStackCdkName: 'mijnnijmegen-pipeline-acceptance',
-    pipelineName: 'mijnnijmegen-acceptance',
-  },
-  'production-new-lz': {
-    branch: 'production',
-    buildEnvironment: Statics.gnBuildEnvironment,
-    deploymentEnvironment: Statics.gnMijnNijmegenProdEnvironment,
-    cnameRecords: {
-      _abe87d0d7f8458c5f75c5d1e0bf6efdb: '_0d3e717e52354c47bf6b0c16612b709d.jzckvmdcqj.acm-validations.aws.',
-    },
-    dsRecord: '40951 13 2 1EFF20C0264CD1FDE6C7C858398BC2141768CC014A7BB27997F323076B7C47ED',
-    pipelineStackCdkName: 'mijnnijmegen-pipeline-production',
-    pipelineName: 'mijnnijmegen-production',
-  },
 };
 
 export function getEnvironmentConfiguration(branchName: string) {
