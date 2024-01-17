f666bbba7307b7d793e85f3849a71b4c324b967b
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-01-17 17:03:02 +0100

    fux test

--- src/shared/tests/nav.test.ts
+++ src/shared/tests/nav.test.ts
@@ -1,21 +1,21 @@
 import { Navigation } from '../Navigation';
 describe('Navigation construction', () => {
-  test('Regular navigation retrieves 2 items', async () => {
+  test('Regular navigation retrieves 3 items', async () => {
     const navigation = new Navigation('person');
-    expect(navigation.items).toHaveLength(2);
+    expect(navigation.items).toHaveLength(3);
   });
   test('Organisation navigation retrieves no items', async () => {
     const navigation = new Navigation('organisation');
-    expect(navigation.items).toHaveLength(0);
+    expect(navigation.items).toHaveLength(1);
   });
 
   test('Organisation navigation with zaken retrieves 1 item', async () => {
     const navigation = new Navigation('organisation', { showZaken: true });
-    expect(navigation.items).toHaveLength(1);
+    expect(navigation.items).toHaveLength(2);
   });
 
   test('Person navigation with zaken retrieves 3 item', async () => {
     const navigation = new Navigation('person', { showZaken: true });
-    expect(navigation.items).toHaveLength(3);
+    expect(navigation.items).toHaveLength(4);
   });
 });
