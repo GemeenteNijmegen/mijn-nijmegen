17accdc03bc512464cdfda5c7d556f84df848140
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-06-04 14:07:07 +0200

    feat: Add id token (#1142)
    Adds the tokens to the session object, so we can
    use it for calls in the future.

--- src/app/auth/AuthRequestHandler.ts
+++ src/app/auth/AuthRequestHandler.ts
@@ -44,7 +44,6 @@ export class AuthRequestHandler {
     let session = new Session(this.config.cookies, this.config.dynamoDBClient);
     await session.init();
     if (session.sessionId === false) {
-
       return Response.redirect('/login');
     }
     const state = session.getValue('state');
@@ -68,6 +67,8 @@ export class AuthRequestHandler {
           bsn: { S: user.type == 'person' ? user.identifier : '' }, // TODO: remove when consuming pages (persoonsgegevens, uitkeringen, zaken) have been updated to use identifier
           user_type: { S: user.type },
           username: { S: username },
+          id_token: { S: tokens.id_token },
+          refresh_token: { S: tokens.refresh_token },
         });
       } catch (error: any) {
         console.error('creating session failed', error);
