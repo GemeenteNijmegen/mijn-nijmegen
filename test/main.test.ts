66fb25511e447e14294699b07a95e2398a883655
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2024-07-25 16:22:05 +0200

    fix: set correct parameter amount in test

--- test/main.test.ts
+++ test/main.test.ts
@@ -92,7 +92,7 @@ test('StackHasParameters', () => {
   const app = new App();
   const stack = new ParameterStack(app, 'test');
   const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::SSM::Parameter', 23);
+  template.resourceCountIs('AWS::SSM::Parameter', 24);
 });
 
 
