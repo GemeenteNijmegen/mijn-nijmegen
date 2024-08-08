348732f710aa352580785c5793dc6cb93190e7ed
Marnix Dessing <m.dessing@nijmegen.nl>
2024-08-08 11:20:01 +0200

    feat: get and download zaken via zaakaggregator

--- src/app/zaken/zakenRequestHandler.ts
+++ src/app/zaken/zakenRequestHandler.ts
@@ -7,7 +7,7 @@ import * as zaakTemplate from './templates/zaak.mustache';
 import * as zakenTemplate from './templates/zaken.mustache';
 import { User, UserFromSession } from './User';
 import { ZaakAggregator } from './ZaakAggregator';
-import { ZaakSummary } from './ZaakConnector';
+import { SingleZaak, ZaakSummary } from './ZaakConnector';
 import { ZaakFormatter } from './ZaakFormatter';
 import { Navigation } from '../../shared/Navigation';
 import { render } from '../../shared/render';
@@ -15,7 +15,8 @@ import { render } from '../../shared/render';
 export class ZakenRequestHandler {
   private zaakAggregator: ZaakAggregator;
   private dynamoDBClient: DynamoDBClient;
-  private apikey?: string;
+  private apiKey?: string;
+
   constructor(zaakAggregator: ZaakAggregator, dynamoDBClient: DynamoDBClient) {
     this.zaakAggregator = zaakAggregator;
     this.dynamoDBClient = dynamoDBClient;
@@ -68,17 +69,14 @@ export class ZakenRequestHandler {
   }
 
   private async fetchList(user: User) {
-    if (!process.env.APIGATEWAY_BASEURL || !process.env.APIGATEWAY_APIKEY) {
-      throw Error('not all required env variables are set');
-    }
-    this.apikey = await AWS.getSecret(process.env.APIGATEWAY_APIKEY);
+    const key = await this.getApiKey();
     const response = await fetch(`${process.env.APIGATEWAY_BASEURL}zaken?` + new URLSearchParams({
       userType: user.type,
       userIdentifier: user.identifier,
     }).toString(), {
       method: 'GET',
       headers: {
-        'x-api-key': this.apikey,
+        'x-api-key': key,
       },
     });
     return ZaakSummariesSchema.parse(await response.json());
@@ -86,7 +84,14 @@ export class ZakenRequestHandler {
 
   async get(zaakConnectorId: string, zaakId: string, session: Session) {
     const user = UserFromSession(session);
-    const zaak = await this.zaakAggregator.get(zaakId, zaakConnectorId, user);
+
+    let zaak;
+    if (process.env.APIGATEWAY_BASEURL && process.env.APIGATEWAY_APIKEY) {
+      zaak = await this.fetchGet(zaakId, zaakConnectorId, user);
+    } else {
+      zaak = await this.zaakAggregator.get(zaakId, zaakConnectorId, user);
+    }
+
     if (zaak) {
       const formattedZaak = new ZaakFormatter().formatZaak(zaak);
 
@@ -106,15 +111,72 @@ export class ZakenRequestHandler {
     }
   }
 
+  private async fetchGet(zaakId: string, zaakConnectorId: string, user: User) {
+    const key = await this.getApiKey();
+
+    const response = await fetch(`${process.env.APIGATEWAY_BASEURL}zaken/${zaakConnectorId}/${zaakId}?` + new URLSearchParams({
+      userType: user.type,
+      userIdentifier: user.identifier,
+    }).toString(), {
+      method: 'GET',
+      headers: {
+        'x-api-key': key,
+      },
+    });
+    const json = await response.json() as any;
+    json.registratiedatum = new Date(json.registratiedatum);
+    json.verwachtte_einddatum = new Date(json.verwachtte_einddatum);
+    json.uiterlijke_einddatum = new Date(json.uiterlijke_einddatum);
+    json.einddatum = json.einddatum ? new Date(json.einddatum) : undefined;
+    return json as SingleZaak;
+  }
+
   async download(zaakConnectorId: string, zaakId: string, file: string, session: Session) {
     const user = UserFromSession(session);
-    const response = await this.zaakAggregator.download(zaakConnectorId, zaakId, file, user);
+
+    let response;
+    if (process.env.APIGATEWAY_BASEURL && process.env.APIGATEWAY_APIKEY) {
+      response = await this.fetchDownload(zaakId, zaakConnectorId, file, user);
+    } else {
+      response = await this.zaakAggregator.download(zaakConnectorId, zaakId, file, user);
+    }
+
     if (response) {
       return Response.redirect(response.downloadUrl);
     } else {
       return Response.error(404);
     }
   }
+
+  private async fetchDownload(zaakId: string, zaakConnectorId: string, file: string, user: User) {
+    const key = await this.getApiKey();
+
+    const response = await fetch(`${process.env.APIGATEWAY_BASEURL}zaken/${zaakConnectorId}/${zaakId}/download/${file}?` + new URLSearchParams({
+      userType: user.type,
+      userIdentifier: user.identifier,
+    }).toString(), {
+      method: 'GET',
+      headers: {
+        'x-api-key': key,
+      },
+    });
+    const json = await response.json() as any;
+    return json;
+  }
+
+  private async getApiKey(): Promise<string> {
+    if (!this.apiKey) {
+      if (!process.env.APIGATEWAY_BASEURL || !process.env.APIGATEWAY_APIKEY) {
+        throw Error('not all required env variables are set');
+      }
+      this.apiKey = await AWS.getSecret(process.env.APIGATEWAY_APIKEY);
+      if (!this.apiKey) {
+        throw Error('No API key found');
+      }
+    }
+    return this.apiKey;
+  }
+
 }
 
 export const ZaakSummarySchema = z.object({
