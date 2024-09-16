ebfd52f29c62f949c704f272ec5a24487219b03e
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-09-16 13:22:40 +0200

    feat: Add new csp directives
    Adds the form-action and base-uri csp directives, both with value 'self'
    No base-element is used in the application, this prevents incorrectly
    set uri's. Similarly, the only forms submitted on the site (none so far)
    should have the site itself as the target.

--- src/CloudfrontStack.ts
+++ src/CloudfrontStack.ts
@@ -299,17 +299,19 @@ export class CloudfrontStack extends Stack {
    * @returns string csp header values
    */
   cspHeaderValue() {
-    const cspValues = 'default-src \'self\';\
-    frame-ancestors \'none\';\
-    frame-src \'self\';\
-    connect-src \'self\' https://componenten.nijmegen.nl;\
-    style-src \'self\' https://componenten.nijmegen.nl https://fonts.googleapis.com https://fonts.gstatic.com \
-    \'sha256-hS1LM/30PjUBJK3kBX9Vm9eOAhQNCiNhf/SCDnUqu14=\' \'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=\' \'sha256-OTeu7NEHDo6qutIWo0F2TmYrDhsKWCzrUgGoxxHGJ8o=\';\
-    script-src \'self\' https://componenten.nijmegen.nl https://siteimproveanalytics.com;\
-    font-src \'self\' https://componenten.nijmegen.nl https://fonts.gstatic.com;\
-    img-src \'self\' https://componenten.nijmegen.nl data: https://*.siteimproveanalytics.io;\
-    object-src \'none\';\
-    ';
+    const cspValues = `default-src 'self';
+frame-ancestors 'none';
+frame-src 'self';
+base-uri 'self';
+form-action 'self';
+connect-src 'self' https://componenten.nijmegen.nl;
+style-src 'self' https://componenten.nijmegen.nl https://fonts.googleapis.com https://fonts.gstatic.com 
+'sha256-hS1LM/30PjUBJK3kBX9Vm9eOAhQNCiNhf/SCDnUqu14=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-OTeu7NEHDo6qutIWo0F2TmYrDhsKWCzrUgGoxxHGJ8o=';
+script-src 'self' https://componenten.nijmegen.nl https://siteimproveanalytics.com;
+font-src 'self' https://componenten.nijmegen.nl https://fonts.gstatic.com;
+img-src 'self' https://componenten.nijmegen.nl data: https://*.siteimproveanalytics.io;
+object-src 'none';
+`;
     return cspValues.replace(/[ ]+/g, ' ').trim();
   }
 
