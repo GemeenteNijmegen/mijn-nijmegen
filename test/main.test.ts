e7d24d3900461f4499529fb4234a6040ce83073b
Marnix Dessing <m.dessing@nijmegen.nl>
2025-01-27 13:51:10 +0100

    chore: bump infra test count

--- test/main.test.ts
+++ test/main.test.ts
@@ -91,7 +91,7 @@ test('StackHasParameters', () => {
   const app = new App();
   const stack = new ParameterStack(app, 'test');
   const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::SSM::Parameter', 30);
+  template.resourceCountIs('AWS::SSM::Parameter', 33);
 });
 
 
@@ -99,7 +99,7 @@ test('StackHasSecrets', () => {
   const app = new App();
   const stack = new ParameterStack(app, 'test');
   const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::SecretsManager::Secret', 12);
+  template.resourceCountIs('AWS::SecretsManager::Secret', 14);
 });
 
 
