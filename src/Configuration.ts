5c2581c90760ab055aa4eea87b17e88a1d3c8dc2
Marnix Dessing <m.dessing@nijmegen.nl>
2025-01-27 10:56:05 +0100

    feat: enable nl wallet in production

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -193,8 +193,8 @@ const EnvironmentConfigurations: {[key:string]: Configuration} = {
     zakenUseSubmissions: true,
     inzageLive: false,
     useZakenFromAggregatorAPI: true,
-    nlWalletVerIdIsLive: false,
-    nlWalletSignicatIsLive: false,
+    nlWalletVerIdIsLive: true,
+    nlWalletSignicatIsLive: true,
   },
 };
 
