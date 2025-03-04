161a5dcb521ee7157deece24fe6bed3da436b998
Marnix Dessing <m.dessing@nijmegen.nl>
2025-03-04 10:23:56 +0100

    chore: add tests and date mapping

---
+++ src/app/persoonsgegevens/tests/mapper.test.ts
@@ -0,0 +1,58 @@
+import { PersoonsgegevensMapper } from "../Persoonsgegevens";
+
+describe('Geboortedatum mapping', () => {
+
+  test('to dutch', () => {
+    const input = '1950-09-19'
+    const expected = '19-09-1950';
+    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
+  });
+
+  test('unknown', () => {
+    const input = '0000-00-00'
+    const expected = '00-00-0000';
+    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
+  });
+
+  test('unknown month and day', () => {
+    const input = '1950-00-00'
+    const expected = '00-00-1950';
+    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
+  });
+
+})
+
+
+describe('Nationaliteit', () => {
+
+  const dutch =
+  {
+    "type": "Nationaliteit",
+    "nationaliteit": {
+      "code": "6030",
+      "omschrijving": "Nederlandse"
+    },
+  };
+
+  const indian = {
+    "type": "Nationaliteit",
+    "nationaliteit": {
+      "code": "0312",
+      "omschrijving": "Indiase"
+    },
+  };
+
+  test('Empty', () => {
+    expect(PersoonsgegevensMapper.hasNederlandseNationaliteit(undefined)).toBe('Nee')
+  });
+
+  test('With NL nationality', () => {
+    expect(PersoonsgegevensMapper.hasNederlandseNationaliteit([dutch, indian])).toBe('Ja')
+  });
+
+  test('Without NL nationality', () => {
+    expect(PersoonsgegevensMapper.hasNederlandseNationaliteit([indian])).toBe('Nee')
+  });
+
+
+})
