802bf7e243e97a4678b0db5626eba8c5efa747d8
Marnix Dessing <m.dessing@nijmegen.nl>
2025-03-04 12:02:15 +0100

    fix: use langFormat for geboortedatum haalcentraal

--- src/app/persoonsgegevens/tests/mapper.test.ts
+++ src/app/persoonsgegevens/tests/mapper.test.ts
@@ -1,28 +1,5 @@
 import { PersoonsgegevensMapper } from '../Persoonsgegevens';
 
-describe('Geboortedatum mapping', () => {
-
-  test('to dutch', () => {
-    const input = '1950-09-19';
-    const expected = '19-09-1950';
-    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
-  });
-
-  test('unknown', () => {
-    const input = '0000-00-00';
-    const expected = '00-00-0000';
-    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
-  });
-
-  test('unknown month and day', () => {
-    const input = '1950-00-00';
-    const expected = '00-00-1950';
-    expect(PersoonsgegevensMapper.isoDateToDutchFormat(input)).toBe(expected);
-  });
-
-});
-
-
 describe('Nationaliteit', () => {
 
   const dutch =
