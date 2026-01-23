e4e2e8a3396e0b9f8f28d9fbebd6e23ab48dfc6d
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-23 14:42:56 +0100

    chore: rename secret to not end in -secret

--- src/statics.ts
+++ src/statics.ts
@@ -38,7 +38,7 @@ export abstract class Statics {
    * OpenID Connect configuration
    */
   static readonly _OIDCClientID: string = '/cdk/mijn-nijmegen/oidc/client-id';
-  static readonly _OIDCClientSecret: string = '/cdk/mijn-nijmegen/oidc/client-secret';
+  static readonly _OIDCClientSecret: string = '/cdk/mijn-nijmegen/oidc/clientsecret';
   static readonly _OIDCClientWellKnown: string = '/cdk/mijn-nijmegen/oidc/well-knonw-url';
   static readonly _OIDCClientRedirectUrl: string = '/cdk/mijn-nijmegen/oidc/redirect-url';
 
