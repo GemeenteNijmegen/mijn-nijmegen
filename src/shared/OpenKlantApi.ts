c829a56fcd1d6d5d28f2b7f54ea8304b62f24013
Marnix Dessing <m.dessing@nijmegen.nl>
2026-02-27 10:43:05 +0100

    chore: open-klant api and parameters passed to lambda

---
+++ src/shared/OpenKlantApi.ts
@@ -0,0 +1,61 @@
+import { Bsn } from '@gemeentenijmegen/utils';
+import { ApiClient } from './ApiClient';
+
+interface Config {
+  readonly baseUrl: string;
+  readonly apiclient: ApiClient;
+}
+
+interface DigitaalAdres {
+  adres: string;
+  soortDigitaalAdres: 'email' | 'telefoonnummer';
+}
+
+interface Partij {
+  uuid: string;
+  digitaleAdressen?: DigitaalAdres[];
+}
+
+interface PartijResponse {
+  count: number;
+  results: Partij[];
+}
+
+export interface ContactInfo {
+  email?: string;
+  phonenumber?: string;
+}
+
+export class OpenKlantApi {
+
+  constructor(private config: Config) { }
+
+  async getContactInfo(identifier: string, type: 'person' | 'organisation'): Promise<ContactInfo> {
+    const filterField = type === 'person'
+      ? 'partijIdentificator__codeSoortObjectId=bsn'
+      : 'partijIdentificator__codeSoortObjectId=kvk';
+
+    const url = `${this.config.baseUrl}/klantinteracties/api/v1/partijen?${filterField}&partijIdentificator__objectId=${identifier}&expand=digitaleAdressen`;
+
+    const data: PartijResponse = await this.config.apiclient.getData(url);
+
+    if (!data?.results || data.results.length === 0) {
+      throw Error('No partij found for identifier');
+    }
+
+    const partij = data.results[0];
+    const contactInfo: ContactInfo = {};
+
+    if (partij.digitaleAdressen) {
+      for (const adres of partij.digitaleAdressen) {
+        if (adres.soortDigitaalAdres === 'email' && !contactInfo.email) {
+          contactInfo.email = adres.adres;
+        } else if (adres.soortDigitaalAdres === 'telefoonnummer' && !contactInfo.phonenumber) {
+          contactInfo.phonenumber = adres.adres;
+        }
+      }
+    }
+
+    return contactInfo;
+  }
+}
