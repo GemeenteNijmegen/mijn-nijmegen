21c6c8b7adb19278eabcbe9fc13d9cafab4136ff
Marnix Dessing <m.dessing@nijmegen.nl>
2026-09-02 16:24:19 +0200

    feat: login method groups

--- src/app/login/loginRequestHandler.ts
+++ src/app/login/loginRequestHandler.ts
@@ -1,9 +1,12 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { ApiGatewayV2Response, Response } from '@gemeentenijmegen/apigateway-http/lib/V2/Response';
-import { Session } from '@gemeentenijmegen/session';
-import * as loginTemplate from './templates/login.mustache';
-import { OpenIDConnect } from '../../shared/OpenIDConnect';
-import { render } from '../../shared/render';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  ApiGatewayV2Response,
+  Response,
+} from "@gemeentenijmegen/apigateway-http/lib/V2/Response";
+import { Session } from "@gemeentenijmegen/session";
+import { OpenIDConnect } from "../../shared/OpenIDConnect";
+import { render } from "../../shared/render";
+import * as loginTemplate from "./templates/login.mustache";
 
 interface LoginRequestHandlerProps {
   /**
@@ -57,31 +60,36 @@ interface AuthMethod {
   methodNiceName: string;
 }
 
-export class LoginRequestHandler {
+interface AuthMethodGroup {
+  groupName: string;
+  authMethods: AuthMethod[];
+}
 
+export class LoginRequestHandler {
   private config: LoginRequestHandlerProps;
 
   constructor(props: LoginRequestHandlerProps) {
     this.config = props;
   }
 
-  async handleRequest(params: RequestParams, dynamoDBClient: DynamoDBClient): Promise<ApiGatewayV2Response> {
+  async handleRequest(
+    params: RequestParams,
+    dynamoDBClient: DynamoDBClient,
+  ): Promise<ApiGatewayV2Response> {
     let session = new Session(params.cookies!, dynamoDBClient);
     await session.init();
     if (session.isLoggedIn() === true) {
-      console.debug('redirect to home');
-      return Response.redirect('/');
+      console.debug("redirect to home");
+      return Response.redirect("/");
     }
 
     if (params.method) {
       return this.setupAuthenticationRedirect(session, params.method);
     }
 
-    const authMethods = this.addAuthMethods();
-
     const data = {
-      title: 'Inloggen',
-      authMethods,
+      title: "Inloggen",
+      authMethodGroups: this.authMethodGroups(),
     };
 
     let template = loginTemplate.default;
@@ -92,15 +100,36 @@ export class LoginRequestHandler {
   private addAuthMethods(): AuthMethod[] {
     const authMethods = [];
     if (this.config?.digidScope) {
-      authMethods.push(this.authMethodData('digid', 'DigiD'));
+      authMethods.push(this.authMethodData("digid", "DigiD"));
     }
     if (this.config?.yiviScope) {
-      authMethods.push(this.authMethodData('yivi', 'Yivi'));
+      authMethods.push(this.authMethodData("yivi", "Yivi"));
     }
     if (this.config?.eHerkenningScope) {
-      authMethods.push(this.authMethodData('eherkenning', 'eHerkenning'));
+      authMethods.push(this.authMethodData("eherkenning", "eHerkenning"));
     }
     return authMethods;
+  }
+
+  /**
+   * Splits the configured auth methods into audience columns for the login page:
+   * one for logging in as yourself (DigiD, Yivi, eHerkenning), one for logging in on behalf of
+   * someone else (to be implemented). Groups without any configured method are omitted.
+   *
+   * Later can be extended for fieldlab experiment for gemachtigde.
+   */
+  private authMethodGroups(): AuthMethodGroup[] {
+    const methods = this.addAuthMethods();
+    const methodsNamed = (names: string[]) =>
+      methods.filter((method) => names.includes(method.methodName));
+    const groups: AuthMethodGroup[] = [
+      {
+        groupName: "", // No name for now
+        authMethods: methodsNamed(["digid", "yivi", "eherkenning"]),
+      },
+      // { groupName: 'Inloggen namens iemand anders', authMethods: methodsNamed([]) },
+    ];
+    return groups.filter((group) => group.authMethods.length > 0);
   }
 
   private authMethodData(name: string, niceName: string) {
@@ -111,9 +140,11 @@ export class LoginRequestHandler {
   }
 
   private async setupAuthenticationRedirect(session: Session, method: string) {
-    const supportedMethod = this.addAuthMethods().find(known => known.methodName == method);
+    const supportedMethod = this.addAuthMethods().find(
+      (known) => known.methodName == method,
+    );
     if (!supportedMethod) {
-      throw Error('Authentication method is not suported!');
+      throw Error("Authentication method is not suported!");
     }
 
     const baseOidcScope = this.config.oidcScope;
@@ -127,29 +158,37 @@ export class LoginRequestHandler {
 
     let loginUrl = undefined;
     switch (method) {
-      case 'yivi':
+      case "yivi":
         const yiviScope = [baseOidcScope, this.config.yiviScope];
-        if (this.config?.useYiviKvk) { // Feature flag
-          yiviScope.push(this.config.yiviCondisconScope ?? '');
+        if (this.config?.useYiviKvk) {
+          // Feature flag
+          yiviScope.push(this.config.yiviCondisconScope ?? "");
         } else {
-          yiviScope.push(this.config.yiviBsnAttribute ?? '');
+          yiviScope.push(this.config.yiviBsnAttribute ?? "");
         }
-        loginUrl = await this.config.oidc.getLoginUrl(state, yiviScope.join(' '));
+        loginUrl = await this.config.oidc.getLoginUrl(
+          state,
+          yiviScope.join(" "),
+        );
         break;
-      case 'digid':
-        loginUrl = await this.config.oidc.getLoginUrl(state, `${baseOidcScope} ${this.config.digidScope}`);
+      case "digid":
+        loginUrl = await this.config.oidc.getLoginUrl(
+          state,
+          `${baseOidcScope} ${this.config.digidScope}`,
+        );
         break;
-      case 'eherkenning':
-        loginUrl = await this.config.oidc.getLoginUrl(state, `${baseOidcScope} ${this.config.eHerkenningScope}`);
+      case "eherkenning":
+        loginUrl = await this.config.oidc.getLoginUrl(
+          state,
+          `${baseOidcScope} ${this.config.eHerkenningScope}`,
+        );
         break;
     }
 
     if (!loginUrl) {
-      throw Error('Unsupported auth method.');
+      throw Error("Unsupported auth method.");
     }
 
     return Response.redirect(loginUrl.toString(), 302, session.getCookie());
-
   }
-
 }
